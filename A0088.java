efe83240fe4ccaa20d66626b6ae6412d6f6b0148
jinshugusui <291691357@qq.com>
2019-08-08 06:31:48 +0800

    Add files via upload

--- A0088.java
+++ A0088.java
@@ -10,7 +10,8 @@ public class A0088 {
 		int m = 3;
 		int n = 3;
 		
-		merge(nums1, m, nums2, n);
+		A0088 test = new A0088();
+		test.merge(nums1, m, nums2, n);
 		
 		for(int z:nums1){
 			System.out.print(z+",");	
@@ -19,9 +20,9 @@ public class A0088 {
 	}
 	
 	
-    public static  void merge(int[] nums1, int m, int[] nums2, int n) {
+    public  void merge(int[] nums1, int m, int[] nums2, int n) {
     	
-    	//最直观的办法
+    	
     	//取一个新的数组a容量为m+n,将nums1和nums2的实际数据都放到新数组中，排序新数组
     	//将a数组中的内容，覆盖到nums1
 /*    	
@@ -49,22 +50,21 @@ public class A0088 {
     	
     	//nums1末尾和nums2末尾比大小，大的放在nums1的m+n位置上，且对应数组索引前进1，
     	//某一个数组索引为0后，由另外一个数组直接替代。
-/*    	
+    	
     	int tail = m + n - 1;
     	while(tail >= 0){
-    		if (m) {
-    			
-    		} else if (){
-    			
-    		} else if () {
-    			
+    		if (m < 0 && n >= 0) {
+    			nums1[n] = nums2[n];
+    			n--;
+    		} else if (nums1[m-1] >= nums2[n-1]) {
+    			nums1[tail] = 
     		} else {
     			
     		}
     		
     		tail--;
     	}
-*/    	
+   	
     }
     
     
