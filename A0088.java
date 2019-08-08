425a4252220887139d4fd3c68b2033e316d524c8
jinshugusui <291691357@qq.com>
2019-08-08 09:28:34 +0800

    Add files via upload

--- A0088.java
+++ A0088.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 public class A0088 {
 
 	public static void main(String[] args) {
-		int[] nums1  = {1,2,3,0,0,0,0};
-		int[] nums2  = {2,5,6};
-		int m = 3;
-		int n = 3;
+		int[] nums1  = {0};
+		int[] nums2  = {2};
+		int m = 0;
+		int n = 1;
 		
 		A0088 test = new A0088();
 		test.merge(nums1, m, nums2, n);
@@ -21,8 +21,9 @@ public class A0088 {
 	
 	
     public  void merge(int[] nums1, int m, int[] nums2, int n) {
+    
     	
-    	
+    	//方法1
     	//取一个新的数组a容量为m+n,将nums1和nums2的实际数据都放到新数组中，排序新数组
     	//将a数组中的内容，覆盖到nums1
 /*    	
@@ -45,21 +46,33 @@ public class A0088 {
 				nums1[i] = temp[i];				
 			}
 		}
-
  */   	
     	
+    	
+    	
+    	
+    	
+    	//方法二
     	//nums1末尾和nums2末尾比大小，大的放在nums1的m+n位置上，且对应数组索引前进1，
     	//某一个数组索引为0后，由另外一个数组直接替代。
-    	
+/*    	
     	int tail = m + n - 1;
     	while(tail >= 0){
-    		if (m < 0 && n >= 0) {
-    			nums1[n] = nums2[n];
+    		if (m <= 0 && n >= 0) {
+    			//nums1用尽，由nums2补充，                                                      	   
+    			nums1[n-1] = nums2[n-1];
     			n--;
+    		} else if (m > 0 && n <= 0){
+    			//若nums2用尽，跳出循环
+    			break;
     		} else if (nums1[m-1] >= nums2[n-1]) {
-    			nums1[tail] = 
-    		} else {
+    			nums1[tail] = nums1[m-1];
+    			nums1[m-1] = 0;
+    			m = m - 1;
     			
+    		} else {
+    			nums1[tail] = nums2[n-1];
+    			n = n - 1;    			
     		}
     		
     		tail--;
@@ -67,8 +80,8 @@ public class A0088 {
    	
     }
     
+*/    
     
-    
-    
+    }   
 
 }
